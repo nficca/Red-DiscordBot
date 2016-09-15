5c82eaa00017ee7b7a3ff2395366a8eddbb52cf1
Nic Ficca <nicficca@gmail.com>
2016-09-14 20:51:09 -0400

    Made subtle changes for testing purposes

--- cogs/general.py
+++ cogs/general.py
@@ -19,13 +19,14 @@ class General:
         self.ball = ["As I see it, yes", "It is certain", "It is decidedly so", "Most likely", "Outlook good",
                      "Signs point to yes", "Without a doubt", "Yes", "Yes – definitely", "You may rely on it", "Reply hazy, try again",
                      "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again",
-                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+                     "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful", 
+                     "Not sure, but one thing I do know is that Nic is a cool dude"]
         self.poll_sessions = []
 
     @commands.command(hidden=True)
     async def ping(self):
         """Pong."""
-        await self.bot.say("Pong.")
+        await self.bot.say("Pong!")
 
     @commands.command()
     async def choose(self, *choices):
